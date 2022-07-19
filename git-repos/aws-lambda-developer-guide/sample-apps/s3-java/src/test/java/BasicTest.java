0bbb8f3b71137a05990d947acdc139a58113b119
sohah <husseinsoh@gmail.com>
2022-07-19 12:53:13 -0500

    fixing pom to include aws dependency, and using configurable fuzz directory for the fuzzing

--- git-repos/aws-lambda-developer-guide/sample-apps/s3-java/src/test/java/BasicTest.java
+++ git-repos/aws-lambda-developer-guide/sample-apps/s3-java/src/test/java/BasicTest.java
@@ -2,6 +2,7 @@ import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.pholser.junit.quickcheck.From;
 import example.Handler;
 import serverfuzz.Config;
+import serverfuzz.StatisticsManager;
 import serverfuzz.env.Environment;
 import serverfuzz.env.Event;
 import edu.berkeley.cs.jqf.fuzz.Fuzz;
@@ -19,40 +20,39 @@ import java.util.Arrays;
 @RunWith(JQF.class)
 public class BasicTest {
 
-    static int testCount = 0;
-
     @BeforeClass
     public static void setupEnv() {
         LambdaFuzzingInput.setupS3EventLambda("example.Handler",
                 "String example.Handler.handleRequest(com.amazonaws.services.lambda.runtime.events.S3Event,com.amazonaws.services.lambda.runtime.Context)",
                 LambdaFuzzingInput.S3EventTypes.OBJECT_CREATED_PUT, "my-bucket");
-        Environment.initFUZZ_DIR();
         Config.isDebugModeOn = true;
         Config.allowErroneousGeneration = true;
         Config.allowFilesOfType = new FileTypeUtil.FileType[]{FileTypeUtil.FileType.IMGE};
         Config.allowedImageExtensions = Arrays.stream(FileTypeUtil.ImageExtensions.values()).map(t -> t.toString()).toArray(String[]::new); //allowing all image formats
+        Config.FUZZDIR="inbound";
+        Environment.initFUZZ_DIR();
     }
 
     @After
     public void resetState() {
         Environment.reset();
-        testCount++;
+        StatisticsManager.testCasesCount++;
     }
 
 
     @Fuzz
     public void fuzzLambdaS3Event(@From(EnvironmentGenerator.class) Event env) throws IllegalArgumentException {
-        System.out.println("before running JQF Testcase " + testCount);
 
         (new Handler()).handleRequest((S3Event) env.event, null);
-        System.out.println("after running JQF Testcase " + testCount);
+        System.out.printf("finished testcase = %d, erroneous generation = %d, no extension = %d%n",
+                StatisticsManager.testCasesCount, StatisticsManager.erronousFilesCount, StatisticsManager.noFileExtensionCount);
     }
 
     @Fuzz(repro = "target/fuzz-results/BasicTest/fuzzLambdaS3Event/corpus")
     public void fuzzLambdaS3EventRepro(@From(EnvironmentGenerator.class) Event env) throws IllegalArgumentException {
-        System.out.println("before running JQF Testcase " + testCount);
 
         (new Handler()).handleRequest((S3Event) env.event, null);
-        System.out.println("after running JQF Testcase " + testCount);
+        System.out.printf("finished testcase = %d, erroneous generation = %d, no extension = %d%n",
+                StatisticsManager.testCasesCount, StatisticsManager.erronousFilesCount, StatisticsManager.noFileExtensionCount);
     }
 }
