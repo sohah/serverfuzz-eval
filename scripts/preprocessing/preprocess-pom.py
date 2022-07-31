9df0880775c2843f35eeb44947ffd4635fe47851
sohah <husseinsoh@gmail.com>
2022-07-31 06:25:42 +0200

    adding first finished version for pom preprocessing

--- scripts/preprocessing/preprocess-pom.py
+++ scripts/preprocessing/preprocess-pom.py
@@ -1,11 +1,45 @@
 #!/usr/bin/python
 
+# takes as input the path of the pom.xml to be preporcessed. The outputs are two things: (1) a modified pom.xml to include all
+# preproccessing needed, and (2) the original pom.xml now renamed to pom_orig.xml.
+
 import sys
 import xml.etree.ElementTree as ET
-from bs4 import BeautifulSoup
+import os
 
+#adds the serverfuzz dependency.
+def add_serverfuzz_dependency(pom_name, nsmap_name):
+    dependencies = pom_name.findall('.//m:dependencies', nsmap_name)
+    serverfuzz_dependency = ET.SubElement(dependencies[len(dependencies) - 1], 'dependency')
+    group_id = ET.SubElement(serverfuzz_dependency, 'groupId')
+    group_id.text = 'edu.umn.cs'
+    artifact_id = ET.SubElement(serverfuzz_dependency, 'artifactId')
+    artifact_id.text = 'serverfuzz'
+    version_id = ET.SubElement(serverfuzz_dependency, 'version')
+    version_id.text = '1.0-SNAPSHOT'
+
+#adds both jqf plugin and jacoco plugin
+def add_plugins(pom_name, nsmap_name):
+    plugins = pom_name.findall('.//m:plugins', nsmap_name)
+    jqf_plugin = ET.SubElement(plugins[len(plugins) - 1], 'plugin')
+    group_id = ET.SubElement(jqf_plugin, 'groupId')
+    group_id.text = 'edu.berkeley.cs.jqf'
+    artifact_id = ET.SubElement(jqf_plugin, 'artifactId')
+    artifact_id.text = 'jqf-maven-plugin'
+    version_id = ET.SubElement(jqf_plugin, 'version')
+    version_id.text = '1.9'  # I probably need at some point to point this to a local jar.
+    add_jacoco_plugins(pom_name, nsmap_name)
+
+#adds jacoco plugin
+def add_jacoco_plugins(pom_name, nsmap_name):
+    jacoco_pom = ET.parse('jacoco.xml')
+    jacoco_root = jacoco_pom.getroot()
+    plugins = pom_name.find('.//m:plugins', nsmap_name)
+    plugins.append(jacoco_root)
+
+
+#defines the right version for different dependencies if they are used.
 replace_dependency_map = {
-
     "aws-lambda-java-events": "3.11.0",
     "aws-java-sdk": "1.12.201",
     "aws-lambda-java-runtime-interface-client": "2.1.1",
@@ -14,25 +48,21 @@ replace_dependency_map = {
     "aws-java-sdk-textract": "1.12.253"
 }
 
-# def prettify(elem):
-#     """Return a pretty-printed XML string for the Element.
-#     """
-#     rough_string = ET.tostring(elem, 'utf-8')
-#     reparsed = minidom.parseString(rough_string)
-#     return reparsed.toprettyxml(indent="\t")
 
 args = len(sys.argv)
 if args < 1:
     print('must pass path to xml file to preprocess')
     exit(1)
 
-pom = ET.parse('before/pom.xml')
+pom_filename = sys.argv[1]
+pom = ET.parse(pom_filename)
 
 root = pom.getroot()
 
 nsmap = {'m': 'http://maven.apache.org/POM/4.0.0'}
 ET.register_namespace('', nsmap.get('m'))
 
+#checks all existing dependencies and maps it with the replace_dependency_map to update them to the correct version.
 for mapping in pom.findall('.//m:dependencies/m:dependency', nsmap):
     artifactId = mapping.find('m:artifactId', nsmap).text
     version = mapping.find('m:version', nsmap)
@@ -45,28 +75,12 @@ for mapping in pom.findall('.//m:dependencies/m:dependency', nsmap):
         else:
             version.text = new_version
 
-dependencies = pom.findall('.//m:dependencies', nsmap)
-
-# serverfuzz_dependency = ET.Element('dependency')
-serverfuzz_dependency = ET.SubElement(dependencies[len(dependencies) - 1], 'dependency')
-group_id = ET.SubElement(serverfuzz_dependency, 'groupId')
-group_id.text = 'edu.umn.cs'
-artifactId = ET.SubElement(serverfuzz_dependency, 'artifactId')
-artifactId.text = 'serverfuzz'
-version = ET.SubElement(serverfuzz_dependency, 'version')
-version.text = '1.0-SNAPSHOT'
+add_serverfuzz_dependency(pom, nsmap) #adds serverfuzz dependency.
+add_plugins(pom, nsmap) #adds jqf plugin and jacoco plugin
 
 # ET.tostring(root)
 
-with open('after/pom.xml', 'wb') as f:
+#renames the original pom.xml to pom_orig.xml and generates the modified pom with the name pom.xml under the same path
+os.rename(pom_filename, pom_filename.split(".")[0] + '_orig.xml')
+with open(pom_filename, 'wb') as f:
     pom.write(f)
-
-#
-# print('printing new map:')
-# for mapping in pom.findall('.//m:dependencies/m:dependency', nsmap):
-#     artifactId = mapping.find('m:artifactId', nsmap).text
-#     version = mapping.find('m:version', nsmap)
-#     if version is None:
-#         print(f'artifact:{artifactId}, version:{None}')
-#     else:
-#         print(f'artifact:{artifactId}, version:{version.text}')
